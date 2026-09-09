dfc7e660c894c948dfa42e7a1ba84f58199c8392
yatogahmisora <yatogahmisora@gmail.com>
2026-09-10 01:13:12 +0700

    10 sept 2026

--- routes/accounting.php
+++ routes/accounting.php
@@ -206,10 +206,6 @@ Route::post('/girodibukaspaddgirokoreksi', [GiroDibukaController::class, 'spAddG
 Route::post('/girodibukaspdeletegirokoreksi', [GiroDibukaController::class, 'spDeleteGiroKoreksi']);
 Route::post('/girodibukaspdelete', [GiroDibukaController::class, 'spDelete']);
 
-
-
-
-
 Route::post('/girodibukaspdetail', [GiroDibukaController::class, 'getDetail']);
 Route::get('/girodibukaloadall', [GiroDibukaController::class, 'loadAll']);
 Route::get('/girodibukalistdph', [GiroDibukaController::class, 'listDPH']);
@@ -229,7 +225,6 @@ Route::post('/girodibukaspotorisasi', [GiroDibukaController::class, 'spOtorisasi
 Route::post('/girodibukaspbatalotorisasi', [GiroDibukaController::class, 'spBatalOtorisasi']);
 Route::post('/girodibukadetailCetak', [GiroDibukaController::class, 'getDetailCetak']);
 
- 
 // // PENGAJUAN DPH
 // Route::get('/pengajuandph', [PengajuanDPHController::class, 'index']);
 // Route::post('/pengajuandphspdetail', [PengajuanDPHController::class, 'getDetail']);
@@ -246,7 +241,6 @@ Route::post('/girodibukadetailCetak', [GiroDibukaController::class, 'getDetailCe
 // Route::post('/pengajuandphspotorisasi' , [PengajuanDPHController::class, 'spOtorisasi']);
 // Route::post('/pengajuandphspbatalotorisasi' , [PengajuanDPHController::class, 'spBatalOtorisasi']);
 // Route::post('/pengajuandphdetailCetak', [PengajuanDPHController::class, 'getDetailCetak']);
-
 
 // PENGAJUAN DPH
 Route::get('/pengajuandphtunai', [PengajuanDPHTunaiController::class, 'index']);
